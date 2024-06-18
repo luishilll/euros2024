a1f7c72cd9406995fb23f2e3edc2be826816cac0
sigmammal <luisahill456@gmail.com>
2024-06-18 12:53:39 +0100

    CSV creation
    Modified scraper.py to work for 5 years of Euro data, and create a csv to be interrogated.

--- scraper.py
+++ scraper.py
@@ -2,30 +2,45 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 
-url = "https://fbref.com/en/comps/676/2021/schedule/2021-European-Championship-Scores-and-Fixtures"
-
-response = requests.get(url)
-html = response.content # get website content
-
-soup = BeautifulSoup(html,"html.parser")
-
-table = soup.find("table")
-rows = table.find_all("tr")  # get table and rows
-
-headers = [header.text for header in rows[0].find_all("th")]
-
-data = []
-for row in rows[1:]:
-    cells = row.find_all('td')
-    data.append([cell.text for cell in cells]) # get data from each row td, starting from second row to not include headers
-
-print(headers)
-
-df = pd.DataFrame(data, columns=headers[1:])
-
-print(df["Home"])
-
-df.to_csv("C:\Users\luish\PycharmProjects\euros2024\euros2024",index=False)
+urls = ["https://fbref.com/en/comps/676/2021/schedule/2021-European-Championship-Scores-and-Fixtures"
+    , "https://fbref.com/en/comps/676/2016/schedule/2016-European-Championship-Scores-and-Fixtures",
+        "https://fbref.com/en/comps/676/2012/schedule/2012-European-Championship-Scores-and-Fixtures",
+        "https://fbref.com/en/comps/676/2008/schedule/2008-European-Championship-Scores-and-Fixtures",
+        "https://fbref.com/en/comps/676/2004/schedule/2004-European-Championship-Scores-and-Fixtures"]
 
 
+def get_data(url):
+    response = requests.get(url)
+    html = response.content  # get website content
 
+    soup = BeautifulSoup(html, "html.parser")
+
+    table = soup.find("table")
+    rows = table.find_all("tr")  # get table and rows
+
+    headers = [header.text for header in rows[0].find_all("th") if header.text != "xG"]
+
+    data = []
+    for row in rows[1:]:
+        cells = row.find_all('td')
+        data.append(
+            [cell.text for cell in cells])  # get data from each row td, starting from second row to not include headers
+
+
+    if url == "https://fbref.com/en/comps/676/2021/schedule/2021-European-Championship-Scores-and-Fixtures":
+        for row in data:
+            row.pop(5)
+            row.pop(6)
+        df = pd.DataFrame(data, columns=headers[1:]) # 2021 euros has xG column, so get rid of that so it matches others
+    else:
+        df = pd.DataFrame(data, columns=headers[1:])
+    return df
+
+
+dataframes = [get_data(url) for url in urls]
+for df in dataframes:
+    print(df.columns)
+
+main_table = pd.concat(dataframes, ignore_index=True)
+
+main_table.to_csv(r"C:\Users\luish\PycharmProjects\euros2024\euros2024\data.csv", index=False)
